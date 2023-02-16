b04c06ce4d827d89436b5e575bd5217b4c3b3d99
AngusGMorrison <contact@angus-morrison.com>
2023-02-16 21:19:41 +0000

    Refactor input processing into editor struct
    Adds an editor struct type to encapsulate input key processing.

--- main.go
+++ main.go
@@ -5,20 +5,18 @@ import (
 	"fmt"
 	"io"
 	"os"
-	"unicode"
-	"unicode/utf8"
 
 	"golang.org/x/term"
 )
-
-// Scan at most one UTF-8 character at a time.
-const scanMaxBytes = 4
 
 func main() {
 	if err := run(); err != nil {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
 }
+
+// Scan at most one UTF-8 character at a time.
+const scanMaxBytes = 4
 
 func run() (err error) {
 	// Enable terminal raw mode to process each keypress as it happens.
@@ -31,25 +29,65 @@ func run() (err error) {
 	// the command used to run the program, so we force the line feed.
 	fmt.Print("\r")
 
-	scanner := newScanner(os.Stdin)
-	for scanner.Scan() {
-		if err := scanner.Err(); err != nil {
-			return fmt.Errorf("scan rune: %w", err)
-		}
-
-		r, _ := utf8.DecodeRune(scanner.Bytes())
-		if r == 'q' {
-			break
-		}
-
-		if unicode.IsControl(r) {
-			fmt.Printf("%d\r\n", r)
-		} else {
-			fmt.Printf("%d (%q)\r\n", r, r)
-		}
+	editor := newEditor(os.Stdin, os.Stdout)
+	for editor.processKeypress() {
+	}
+	if err := editor.Err(); err != nil {
+		return err
 	}
 
 	return nil
+}
+
+type editor struct {
+	scanner *bufio.Scanner
+	out     io.Writer
+	err     error
+}
+
+func newEditor(r io.Reader, w io.Writer) *editor {
+	return &editor{
+		scanner: newScanner(r),
+		out:     w,
+	}
+}
+
+func (e *editor) Err() error {
+	return e.err
+}
+
+func (e *editor) processKeypress() bool {
+	rawKey, err := e.readKey()
+	if err != nil {
+		e.err = err
+		return false
+	}
+	if rawKey == nil {
+		return false
+	}
+
+	// Check for commands in the ASCII range before attempting to interpret Unicode.
+	if len(rawKey) == 1 {
+		fmt.Printf("received byte: %d, %d, %d\n\r", rawKey[0], key(rawKey[0]), ctrlChord(keyQuit))
+		switch key(rawKey[0]) {
+		case ctrlChord(keyQuit): // quit
+			fmt.Println("quit signal")
+			return false
+		}
+	}
+
+	return true
+}
+
+func (e *editor) readKey() ([]byte, error) {
+	if ok := e.scanner.Scan(); !ok {
+		if err := e.scanner.Err(); err != nil {
+			return nil, fmt.Errorf("scan rune: %w", err)
+		}
+		return nil, nil
+	}
+
+	return e.scanner.Bytes(), nil
 }
 
 func newScanner(r io.Reader) *bufio.Scanner {
@@ -59,3 +97,20 @@ func newScanner(r io.Reader) *bufio.Scanner {
 	scanner.Split(bufio.ScanRunes)
 	return scanner
 }
+
+const (
+	// ctrlMask can be combined with any other ASCII character code, CHAR, to represent Ctrl-CHAR.
+	// This is because the terminal handles Ctrl combinations by zeroing bits 5 and 6 of CHAR
+	// (indexed from 0).
+	ctrlMask = 0x1f
+)
+
+func ctrlChord(k key) key {
+	return k & ctrlMask
+}
+
+type key byte
+
+const (
+	keyQuit key = 'q'
+)
