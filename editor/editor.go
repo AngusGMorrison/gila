6d414a7a886ce52da7d3309932d2ef4c090b4df0
AngusGMorrison <contact@angus-morrison.com>
2023-02-17 17:33:16 +0000

    Refactor Editor into package editor

---
+++ editor/editor.go
@@ -0,0 +1,172 @@
+package editor
+
+import (
+	"bufio"
+	"fmt"
+	"io"
+)
+
+// Scan at most one UTF-8 character at a time.
+const scanMaxBytes = 4
+
+type key byte
+
+const (
+	keyQuit key = 'q'
+)
+
+type escapeSequence string
+
+const (
+	escClearScreen   escapeSequence = "\x1b[2J"
+	escCursorTopLeft escapeSequence = "\x1b[H"
+)
+
+// Config contains editor configuration data.
+type Config struct {
+	Width, Height uint
+}
+
+// Editor holds the state for a text editor. Its methods run the main loop for reading and writing
+// input to and from a terminal.
+type Editor struct {
+	config   Config
+	scanner  *bufio.Scanner
+	out      *bufio.Writer
+	readErr  error
+	writeErr error
+}
+
+// New returns a new *Editor that reads from r and writes to w.
+func New(r io.Reader, w io.Writer, config Config) *Editor {
+	return &Editor{
+		config:  config,
+		scanner: newScanner(r),
+		out:     bufio.NewWriter(w),
+	}
+}
+
+// Run starts the editor loop. The editor will update the screen and process user input until
+// commanded to quit or an error occurs.
+func (e *Editor) Run() (err error) {
+	defer e.clearFlush()
+
+	for e.refreshScreen() && e.processKeypress() {
+	}
+	if e.readErr != nil {
+		return e.readErr
+	}
+	if e.writeErr != nil {
+		return e.writeErr
+	}
+	return nil
+}
+
+// processKeypress is designed to be called in a tight loop. By returning a boolean, it is easily
+// incorporated into a loop condition. If an error occurs during the refresh, it is saved to
+// (*editor).readErr, and processKeypress returns false.
+func (e *Editor) processKeypress() bool {
+	rawKey, err := e.readKey()
+	if err != nil {
+		e.readErr = err
+		return false
+	}
+	if rawKey == nil {
+		return false
+	}
+
+	// Check for commands in the ASCII range before attempting to interpret Unicode.
+	if len(rawKey) == 1 {
+		switch key(rawKey[0]) {
+		case ctrlChord(keyQuit): // quit
+			return false
+		}
+	}
+
+	return true
+}
+
+func (e *Editor) readKey() ([]byte, error) {
+	if ok := e.scanner.Scan(); !ok {
+		if err := e.scanner.Err(); err != nil {
+			return nil, fmt.Errorf("scan rune: %w", err)
+		}
+		return nil, nil
+	}
+
+	return e.scanner.Bytes(), nil
+}
+
+// refreshScreen is designed to be called in a tight loop. By returning a boolean, it is easily
+// incorporated into a loop condition. If an error occurs during the refresh, it is saved to
+// (*editor).writeErr, and refreshScreen returns false.
+func (e *Editor) refreshScreen() bool {
+	if err := e.clear(); err != nil {
+		e.writeErr = err
+		return false
+	}
+	if err := e.drawRows(); err != nil {
+		e.writeErr = err
+		return false
+	}
+	if err := e.flush(); err != nil {
+		e.writeErr = err
+		return false
+	}
+	return true
+}
+
+func (e *Editor) clearFlush() error {
+	if err := e.clear(); err != nil {
+		return err
+	}
+	return e.flush()
+}
+
+func (e *Editor) clear() error {
+	if _, err := e.out.WriteString(string(escClearScreen)); err != nil {
+		return fmt.Errorf("clear screen: %w", err)
+	}
+	if _, err := e.out.WriteString(string(escCursorTopLeft)); err != nil {
+		return fmt.Errorf("position cursor: %w", err)
+	}
+	return nil
+}
+
+func (e *Editor) flush() error {
+	if err := e.out.Flush(); err != nil {
+		return fmt.Errorf("flush output buffer: %w", err)
+	}
+	return nil
+}
+
+func (e *Editor) drawRows() error {
+	for y := uint(0); y < e.config.Height-1; y++ {
+		if _, err := e.out.WriteString("~\r\n"); err != nil {
+			return fmt.Errorf("write row: %w", err)
+		}
+	}
+	if err := e.out.WriteByte('~'); err != nil { // no newline for final row
+		return fmt.Errorf("write row: %w", err)
+	}
+	return nil
+}
+
+func newScanner(r io.Reader) *bufio.Scanner {
+	scanner := bufio.NewScanner(r)
+	scanBuf := make([]byte, scanMaxBytes)
+	scanner.Buffer(scanBuf, scanMaxBytes)
+	scanner.Split(bufio.ScanRunes)
+	return scanner
+}
+
+const (
+	// ctrlMask can be combined with any other ASCII character code, CHAR, to represent Ctrl-CHAR.
+	// This is because the terminal handles Ctrl combinations by zeroing bits 5 and 6 of CHAR
+	// (indexed from 0).
+	ctrlMask = 0x1f
+)
+
+func ctrlChord(k key) key {
+	return k & ctrlMask
+}
--- main.go
+++ main.go
@@ -1,11 +1,10 @@
 package main
 
 import (
-	"bufio"
 	"fmt"
-	"io"
 	"os"
 
+	"github.com/angusgmorrison/gila/editor"
 	"golang.org/x/term"
 )
 
@@ -14,9 +13,6 @@ func main() {
 		fmt.Fprintf(os.Stderr, "%s\n", err)
 	}
 }
-
-// Scan at most one UTF-8 character at a time.
-const scanMaxBytes = 4
 
 func run() (err error) {
 	// Enable terminal raw mode to process each keypress as it happens.
@@ -33,172 +29,7 @@ func run() (err error) {
 	if err != nil {
 		return fmt.Errorf("get terminal size: %w", err)
 	}
-	config := editorConfig{width: uint(w), height: uint(h)}
-	editor := newEditor(os.Stdin, os.Stdout, config)
-	// Clear the editor screen on exit.
-	defer func() { err = editor.Clear() }()
-
-	for editor.refreshScreen() && editor.processKeypress() {
-	}
-	if err := editor.ReadErr(); err != nil {
-		return err
-	}
-	if err := editor.WriteErr(); err != nil {
-		return err
-	}
-
-	return nil
+	config := editor.Config{Width: uint(w), Height: uint(h)}
+	ed := editor.New(os.Stdin, os.Stdout, config)
+	return ed.Run()
 }
-
-type editorConfig struct {
-	width, height uint
-}
-
-type editor struct {
-	config   editorConfig
-	scanner  *bufio.Scanner
-	out      *bufio.Writer
-	readErr  error
-	writeErr error
-}
-
-func newEditor(r io.Reader, w io.Writer, config editorConfig) *editor {
-	return &editor{
-		config:  config,
-		scanner: newScanner(r),
-		out:     bufio.NewWriter(w),
-	}
-}
-
-func (e *editor) ReadErr() error {
-	return e.readErr
-}
-
-func (e *editor) WriteErr() error {
-	return e.readErr
-}
-
-// processKeypress is designed to be called in a tight loop. By returning a boolean, it is easily
-// incorporated into a loop condition. If an error occurs during the refresh, it is saved to
-// (*editor).readErr, and processKeypress returns false.
-func (e *editor) processKeypress() bool {
-	rawKey, err := e.readKey()
-	if err != nil {
-		e.readErr = err
-		return false
-	}
-	if rawKey == nil {
-		return false
-	}
-
-	// Check for commands in the ASCII range before attempting to interpret Unicode.
-	if len(rawKey) == 1 {
-		switch key(rawKey[0]) {
-		case ctrlChord(keyQuit): // quit
-			return false
-		}
-	}
-
-	return true
-}
-
-func (e *editor) readKey() ([]byte, error) {
-	if ok := e.scanner.Scan(); !ok {
-		if err := e.scanner.Err(); err != nil {
-			return nil, fmt.Errorf("scan rune: %w", err)
-		}
-		return nil, nil
-	}
-
-	return e.scanner.Bytes(), nil
-}
-
-// refreshScreen is designed to be called in a tight loop. By returning a boolean, it is easily
-// incorporated into a loop condition. If an error occurs during the refresh, it is saved to
-// (*editor).writeErr, and refreshScreen returns false.
-func (e *editor) refreshScreen() bool {
-	if err := e.clearScreen(); err != nil {
-		e.writeErr = err
-		return false
-	}
-	if err := e.drawRows(); err != nil {
-		e.writeErr = err
-		return false
-	}
-	if err := e.flush(); err != nil {
-		e.writeErr = err
-		return false
-	}
-	return true
-}
-
-func (e *editor) Clear() error {
-	if err := e.clearScreen(); err != nil {
-		return err
-	}
-	if err := e.flush(); err != nil {
-		return err
-	}
-	return nil
-}
-
-func (e *editor) flush() error {
-	if err := e.out.Flush(); err != nil {
-		return fmt.Errorf("flush output buffer: %w", err)
-	}
-	return nil
-}
-
-func (e *editor) clearScreen() error {
-	if _, err := e.out.WriteString(string(escClearScreen)); err != nil {
-		return fmt.Errorf("clear screen: %w", err)
-	}
-	if _, err := e.out.WriteString(string(escCursorTopLeft)); err != nil {
-		return fmt.Errorf("position cursor: %w", err)
-	}
-	return nil
-}
-
-func (e *editor) drawRows() error {
-	for y := uint(0); y < e.config.height-1; y++ {
-		if _, err := e.out.WriteString("~\r\n"); err != nil {
-			return fmt.Errorf("write row: %w", err)
-		}
-	}
-	if err := e.out.WriteByte('~'); err != nil { // no newline for final row
-		return fmt.Errorf("write row: %w", err)
-	}
-	return nil
-}
-
-func newScanner(r io.Reader) *bufio.Scanner {
-	scanner := bufio.NewScanner(r)
-	scanBuf := make([]byte, scanMaxBytes)
-	scanner.Buffer(scanBuf, scanMaxBytes)
-	scanner.Split(bufio.ScanRunes)
-	return scanner
-}
-
-const (
-	// ctrlMask can be combined with any other ASCII character code, CHAR, to represent Ctrl-CHAR.
-	// This is because the terminal handles Ctrl combinations by zeroing bits 5 and 6 of CHAR
-	// (indexed from 0).
-	ctrlMask = 0x1f
-)
-
-func ctrlChord(k key) key {
-	return k & ctrlMask
-}
-
-type key byte
-
-const (
-	keyQuit key = 'q'
-)
-
-type escapeSequence string
-
-const (
-	escClearScreen   escapeSequence = "\x1b[2J"
-	escCursorTopLeft escapeSequence = "\x1b[H"
-)
