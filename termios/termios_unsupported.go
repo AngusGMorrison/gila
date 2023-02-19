22899af1ae7ed897b69c75c00b69a07235880c74
AngusGMorrison <contact@angus-morrison.com>
2023-02-19 13:02:21 +0000

    Delete unused termios package

--- termios/termios_unsupported.go
+++
@@ -1,7 +0,0 @@
-//go:build aix && !darwin && !dragonfly && !freebsd && !linux && !netbsd && !openbsd && !solaris && !zos
-
-package termios
-
-func init() {
-	panic(fmt.Sprintf("Platform %s/%s is not supported", runtime.GOOS, runtime.GOARCH))
-}
